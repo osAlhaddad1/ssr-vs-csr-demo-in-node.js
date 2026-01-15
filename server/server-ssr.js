415daa8e64405d83a359918b8b3ff2bc41d45c08
osAlhaddad1 <os.alhaddad@student.hu.nl>
2026-01-15 18:21:04 +0100

    final

--- server/server-ssr.js
+++ server/server-ssr.js
@@ -13,7 +13,7 @@ const server = http.createServer((req, res) => {
         <div id="app">
             <h2>${data.title}</h2>
             <ul>${data.items.map(i => `<li>${i}</li>`).join('')}</ul>
-        </div>
+        </div>3
         <p><em>No JavaScript needed to show this list!</em></p>
     </body>
     </html>`;
